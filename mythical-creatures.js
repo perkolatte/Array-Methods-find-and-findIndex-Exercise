a664935c970f3fc3d541b9efccda70ba2b13c149
Ian Isaacson <ian_isaacson@yahoo.com>
2025-05-27 14:43:55 -0700

    Exercise complete!

--- mythical-creatures.js
+++ mythical-creatures.js
@@ -1,7 +1,28 @@
 const mythicalCreatures = [
-	{name: "Dragon", type: "Fire", lastSeen: "Volcano Valley"},
-	{name: "Mermaid", type: "Water", lastSeen: "Coral Caves"},
-	{name: "Unicorn", type: "Land", lastSeen: "Enchanted Forest"},
-	{name: "Griffin", type: "Air", lastSeen: "Highwind Mountains"},
-	{name: "Kraken", type: "Water", lastSeen: "Abyssal Depths"}
+  { name: "Dragon", type: "Fire", lastSeen: "Volcano Valley" },
+  { name: "Mermaid", type: "Water", lastSeen: "Coral Caves" },
+  { name: "Unicorn", type: "Land", lastSeen: "Enchanted Forest" },
+  { name: "Griffin", type: "Air", lastSeen: "Highwind Mountains" },
+  { name: "Kraken", type: "Water", lastSeen: "Abyssal Depths" },
 ];
+
+const firstWaterCreature = mythicalCreatures.find(
+  (creature) => creature.type === "Water"
+);
+
+console.log("First Water-type Creature:", firstWaterCreature);
+
+const griffinIndex = mythicalCreatures.findIndex(
+  (creature) => creature.name === "Griffin"
+);
+
+console.log("Index of Griffin:", griffinIndex);
+
+const firstCreatureLastSeenInEnchantedForest = mythicalCreatures.find(
+  (creature) => creature.lastSeen === "Enchanted Forest"
+);
+
+console.log(
+  "First Creature Last Seen in Enchanted Forest:",
+  firstCreatureLastSeenInEnchantedForest
+);
